bdf2aa6107a96a8c82ce99a6b76fbb47a3f66127
Budi Oktaviyan Suryanto <budioktaviyans@gmail.com>
2018-06-23 13:56:06 +0700

    Add shared prefs sample

--- app/src/main/java/id/kotlin/sample/ktx/sharedprefs/SharedPrefsActivity.kt
+++ app/src/main/java/id/kotlin/sample/ktx/sharedprefs/SharedPrefsActivity.kt
@@ -1,17 +1,25 @@
 package id.kotlin.sample.ktx.sharedprefs
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.MenuItem
+import androidx.core.content.edit
+import androidx.core.widget.toast
 import id.kotlin.sample.ktx.R
 import kotlinx.android.synthetic.main.activity_sharedprefs.*
 
 class SharedPrefsActivity : AppCompatActivity() {
 
+    private lateinit var sharedPreferences: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sharedprefs)
         setSupportActionBar(toolbar_sharedprefs)
+        load()
+        btn_save.setOnClickListener { if (edit_name.text.isNotBlank()) save() }
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
@@ -20,5 +28,21 @@ class SharedPrefsActivity : AppCompatActivity() {
         }
 
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun load() {
+        sharedPreferences = getSharedPreferences("android_ktx_sample", Context.MODE_PRIVATE)
+
+        val text = sharedPreferences.getString("NAME", "")
+        text_name.text = text
+    }
+
+    private fun save() {
+        val text = edit_name.text
+
+        sharedPreferences.edit {
+            putString("NAME", text.toString())
+        }
+        toast("Name has been save!")
     }
 }
