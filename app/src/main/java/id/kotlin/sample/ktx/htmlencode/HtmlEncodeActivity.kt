fdb73cd7da5e692f2419cc37408c08aadcb37a18
Budi Oktaviyan Suryanto <budioktaviyans@gmail.com>
2018-06-23 14:59:35 +0700

    Add html encode sample

--- app/src/main/java/id/kotlin/sample/ktx/htmlencode/HtmlEncodeActivity.kt
+++ app/src/main/java/id/kotlin/sample/ktx/htmlencode/HtmlEncodeActivity.kt
@@ -3,6 +3,7 @@ package id.kotlin.sample.ktx.htmlencode
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.MenuItem
+import androidx.core.text.htmlEncode
 import id.kotlin.sample.ktx.R
 import kotlinx.android.synthetic.main.activity_htmlencode.*
 
@@ -12,6 +13,7 @@ class HtmlEncodeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_htmlencode)
         setSupportActionBar(toolbar_htmlencode)
+        btn_encode.setOnClickListener { encode() }
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
@@ -20,5 +22,12 @@ class HtmlEncodeActivity : AppCompatActivity() {
         }
 
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun encode() {
+        val src = text_html.text
+        val dest = src.toString().htmlEncode()
+
+        text_encode.text = dest
     }
 }
